527e626dc056984e59aee4642e49babd3859897e
AnastasiaRekalo <global-rekalonastya@gmail.com>
2023-10-02 18:13:55 +0300

    Решение 1 задачи

---
+++ Task1/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] GetArray(int row, int colum, int minValue, int maxValue)
+{
+    int[,] array = new int [row, colum];
+    Random rand = new Random();
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < colum; j++)
+        {
+            array[i,j] = rand.Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+
+        }
+        Console.WriteLine(" ");
+    }
+}
+
+
+ int[,] SelectionSort(int[,] array)
+{
+    
+   for (int i = 0; i < array.GetLength(0); i++) 
+   {   
+   for (int k = 0; k < array.GetLength(1); k++)
+   {
+        for (int j = 0; j < array.GetLength(1)-1; j++) 
+        {
+            
+            if(array[i,j]<array[i,j+1]) 
+            {
+            int temp = array[i,j];
+            array[i,j] = array[i,j+1];
+            array[i,j+1] = temp;
+            }
+             
+        }
+   }
+   
+   }
+    return array;
+   }
+   
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+int[,] matrix = GetArray(4, 5, 1, 10);
+PrintArray(matrix);
+Console.WriteLine(" ");
+int[,] newMatrix = SelectionSort(matrix);
+PrintArray(newMatrix);
